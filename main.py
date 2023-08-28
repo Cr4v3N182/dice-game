73228d30e12bdd5e8b828cb9124dcc93141ba82c
i5-PC <mcienkowski@gmail.com>
2023-08-28 20:08:54 +0200

    Add dice info and output result to code.

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import PySimpleGUI as sg
 
 sg.theme("DarkBlue15")
 
+p1_dict = {}
+p2_dict = {}
+
 label = sg.Text("Press roll to get a result")
 p1_roll_button = sg.Button("Player One Roll", key='user1b')
+p1_info = sg.Text("", key='p1info')
 p1_dice_result = sg.Text("Click roll to get a result", key='p1dice')
 p2_roll_button = sg.Button("Player Two Roll", key='user2b')
+p2_info = sg.Text("", key='p2info')
 p2_dice_result = sg.Text("Click roll to get a result", key='p2dice')
-exit_button = sg.Button("Exit", key="exit", pad=40)
+output = sg.Text("", key="output")
+exit_button = sg.Button("Exit", key="exit", pad=15)
 
 layout = [[label],
           [p1_roll_button, p2_roll_button],
           [p1_dice_result, p2_dice_result],
+          [p1_info, p2_info],
+          [output],
           [exit_button]]
 
 window = sg.Window("Dice Game", layout=layout, size=(400,200), element_justification='c', font=("Havetica", 10))
