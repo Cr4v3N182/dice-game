47bab7a601389c5931e9b8949110d61f48ebea9a
i5-PC <mcienkowski@gmail.com>
2023-08-30 23:03:21 +0200

    Add color to p2_roll_dice_button, check result button and window resolution has been changed

--- main.py
+++ main.py
@@ -11,17 +11,19 @@ p1_dice_result = sg.Text("Click roll to get a result", key='p1dice')
 p2_roll_button = sg.Button("Player Two Roll", key='user2b')
 p2_info = sg.Text("", key='p2info')
 p2_dice_result = sg.Text("Click roll to get a result", key='p2dice')
+check_result_button = sg.Button("Check", key='check')
 output = sg.Text("", key='output')
-exit_button = sg.Button("Exit", key="exit", pad=15)
+exit_button = sg.Button("Exit", key="exit")
 
 layout = [[label],
           [p1_roll_button, p2_roll_button],
           [p1_dice_result, p2_dice_result],
+          [check_result_button],
           [p1_info, p2_info],
           [output],
           [exit_button]]
 
-window = sg.Window("Dice Game", layout=layout, size=(400,200), element_justification='c', font=("Havetica", 10))
+window = sg.Window("Dice Game", layout=layout, size=(400,250), element_justification='c', font=("Havetica", 10))
 
 while True:
     event, values = window.read()
@@ -35,7 +37,7 @@ while True:
             p1_dice_result.update(value=p1_list)
         case "user2b":
             roll_dice2()
-            p2_dice_result.update(value=p2_list)
+            p2_dice_result.update(value=p2_list,text_color="red")
 
 
 window.close()
